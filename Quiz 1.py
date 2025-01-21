3ca9dbd678ec0ffa547998fe8c6a3dd73071ff71
Alex-Christianson <ji1200vf@go.minnstate.edu>
2025-01-21 17:28:20 -0600

    Add files via upload

---
+++ Quiz 1.py
@@ -0,0 +1,5 @@
+year = int(input("Enter the year: "))
+month = int(input("Enter the month (1 - 12): "))
+day = int(input("Enter the day (1 - 31): "))
+
+print(f" The date is: {year} - {month} - {day}")
